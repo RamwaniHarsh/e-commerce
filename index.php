60fa6e961752d78a10444636ba190766ede657cf
Harsh Ramwani <53076303+RamwaniHarsh@users.noreply.github.com>
2022-08-18 19:40:30 +0530

    My Project

--- index.php
+++ index.php
@@ -1,38 +1,31 @@
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <style>
-        .error
-        {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        font-weight: bold;
-        background-color: rgb(245, 58, 58);
-        color: white;
-        padding: 10px;
-        width: 100%;
-        border-radius: 5px;  
+        .error{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-weight: bold;
+            background-color: rgb(245, 58, 58);
+            color: white;
+            padding: 10px;
+            width: 100%;
+            border-radius: 5px;  
         }
     </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">  
     <link rel="stylesheet" href="css/style.css">
-         
-    
 </head>
-<body>
-    
+<body style="background-color: #022dba;">
     <div class="container">
         <div class="forms">
             <div class="form login">
                 <span class="title">Login</span>
-            
                 <form action="login.php" method="POST">
                     <div class="input-field">
                         <input type="text" name="u-name" placeholder="Enter your Username" required>
@@ -43,21 +36,17 @@
                         <i class="uil uil-lock icon"></i>
                         <i class="uil uil-eye-slash showHidePw"></i>
                     </div>
-
                     <div class="checkbox-text">
                         <div class="checkbox-content">
                             <input type="checkbox" id="logCheck">
                             <label for="logCheck" class="text">Remember me</label>
                         </div>
-                        
                         <a href="forgot/forgot_ui.php" class="text">Forgot password?</a>
                     </div>
-
                     <div class="input-field button">
                         <input type="submit" value="Login">
                     </div>
                 </form>
-
                 <div class="login-signup">
                     <span class="text">Not a member?
                         <a href="signup/signup.php" class="text signup-link">Signup Now</a>
@@ -65,16 +54,13 @@
 					<div class="error"><?php echo $_GET['error']; ?></div>
                 <?php } ?>
                     </span>
-                    
                 </div>
             </div>
-
-            
-            
         </div>
     </div>
 
-    <script src="js/script.js"></script>
-
+    
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
 </html>
